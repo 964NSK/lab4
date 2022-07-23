140684aabf82e9b0af377f1a2aeb42527aa4847a
Artyom Bryantcev <964nsk@gmail.com>
2022-07-24 00:48:35 +0300

    Updated!

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Triangle triangle1 = new Triangle(Colors.WHITE, "Triangle1", 5,3,4,6,8,9);
+        System.out.println(triangle1.getS());
 
 
 
